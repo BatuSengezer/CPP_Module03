471484433e1b73854896d95ef16aa3ee44fc31c0
BatuSengezer <bsengezer@ku.edu.tr>
2025-03-19 12:13:09 +0100

    added header files

---
+++ ex01/includes/ClapTrap.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsengeze <bsengeze@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 20:19:57 by bsengeze          #+#    #+#             */
+/*   Updated: 2025/03/19 11:56:48 by bsengeze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ClapTrap
+{
+protected:
+    std::string _name;
+    unsigned int _hitPoints;
+    unsigned int _energyPoints;
+    unsigned int _attackDamage;
+
+public:
+    // Orthodox Canonical Form
+    ClapTrap();
+    ClapTrap(const std::string &name);
+    ClapTrap(const ClapTrap &other);
+    ClapTrap &operator=(const ClapTrap &other);
+    ~ClapTrap();
+
+    // Member functions
+    void attack(const std::string &target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+
+    // Getters
+    std::string getName() const;
+    unsigned int getHitPoints() const;
+    unsigned int getEnergyPoints() const;
+    unsigned int getAttackDamage() const;
+};
+
+#endif
---
+++ ex01/includes/ScavTrap.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsengeze <bsengeze@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 11:43:16 by bsengeze          #+#    #+#             */
+/*   Updated: 2025/03/19 11:59:46 by bsengeze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include <iostream>
+#include <string>
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+public:
+    // Orthodox Canonical Form
+    ScavTrap();
+    ScavTrap(const std::string &name);
+    ScavTrap(const ScavTrap &other);
+    ScavTrap &operator=(const ScavTrap &other);
+    ~ScavTrap();
+
+    // Member functions
+    void attack(const std::string &target);
+    void guardGate();
+};
+
+#endif
